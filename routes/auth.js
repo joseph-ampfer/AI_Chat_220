75197de5a77d62e63c02d023bf330287612acd15
Xander <xanderjm1116@gmail.com>
2025-04-19 15:53:23 -0400

    added signups to auth.js and editted original signup html page to work with auth.js, intend to add passport.js functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://xanderjm1116:<NOT_A_PASSWORD>@cluster0.sbcwer2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 // Says this is a router, so use can use router.post(), router.get(), etc
 const router = express.Router();
@@ -7,5 +11,68 @@ const router = express.Router();
 // So router.get('/')  ===  GET '/api/auth/
 // So router.post('/login) === POST '/api/auth/login'
 // etc...
+
+let database;
+
+client.connect()
+    .then(() => {
+        database = client.db('Chat220');
+        console.log('Connected to database')
+        
+    })
+    .catch(err => console.log('Issue connecting to database:', err));
+
+
+router.get('/check-email', async (req, res) => {
+    const {email} = req.query;
+    if (!email) {
+        return res.status(400).json({error: 'Email is required'});
+    }
+    try {
+        const usersCollection = database.collection('users');
+        const existingUser = await usersCollection.findOne({email});
+
+        if (existingUser) {
+            return res.status(200).json({exists: true});
+        }
+        else {
+            return res.status(200).json({exists: false});
+        }
+    }
+    catch (err) {
+        console.log('Error checking email:', err);
+        res.status(500).json({error: 'Error checking email'});
+    }
+});
+
+
+router.post('/signup', async (req, res) => {
+    const {email, password} = req.body;
+
+    //checking that email/password were entered
+    if (!email || !password) {
+        return res.status(400).json({error: 'Email and password are required'});
+    }
+
+    try {
+        const usersCollection = database.collection('users');
+
+        //checking email in use
+        const existingUser = await usersCollection.findOne({email});
+        if (existingUser) {
+            return res.status(400).json({error: 'Email is in use'});
+        }
+
+        
+
+        //adding user to database after checks
+        await usersCollection.insertOne({email, password});
+        res.status(201).json({success: 'User added to database'});
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Error adding to database'});
+    }
+
+});
 
 module.exports = router;
